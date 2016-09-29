9918fce2b88f4bba787a5855dd0a3c33ca649083
ljlight <ljlight@umich.edu>
2016-09-29 16:20:28 -0400

    Views from the 5/6

---
+++ createViews.sql
@@ -0,0 +1,96 @@
+/*
+DESC USER_INFORMATION
+ Name					   Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ USER_ID					    VARCHAR2(100)
+ FIRST_NAME					    VARCHAR2(100)
+ LAST_NAME					    VARCHAR2(100)
+ YEAR_OF_BIRTH					    NUMBER(38)
+ MONTH_OF_BIRTH 				    NUMBER(38)
+ DAY_OF_BIRTH					    NUMBER(38)
+ GENDER 					    VARCHAR2(100)
+ CURRENT_CITY					    VARCHAR2(100)
+ CURRENT_STATE					    VARCHAR2(100)
+ CURRENT_COUNTRY				    VARCHAR2(100)
+ HOMETOWN_CITY					    VARCHAR2(100)
+ HOMETOWN_STATE 				    VARCHAR2(100)
+ HOMETOWN_COUNTRY				    VARCHAR2(100)
+ INSTITUTION_NAME				    VARCHAR2(100)
+ PROGRAM_YEAR					    NUMBER(38)
+ PROGRAM_CONCENTRATION				    CHAR(100)
+ PROGRAM_DEGREE 				    VARCHAR2(100)
+*/
+CREATE VIEW VIEW_USER_INFORMATION (
+
+
+);
+
+/*
+ Name					   Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ USER1_ID					    VARCHAR2(100)
+ USER2_ID					    VARCHAR2(100)
+*/
+CREATE VIEW VIEW_ARE_FRIENDS (USER1_ID, USER2_ID) AS 
+	(SELECT CAST(F.USER1_ID AS VARCHAR2(100)), CAST(F.USER2_ID AS VARCHAR2(100))
+	 FROM FRIENDS AS F);
+
+
+/*
+Name					   Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ALBUM_ID					    VARCHAR2(100)
+ OWNER_ID					    VARCHAR2(100)
+ COVER_PHOTO_ID 				    VARCHAR2(100)
+ ALBUM_NAME					    VARCHAR2(100)
+ ALBUM_CREATED_TIME				    TIMESTAMP(6)
+ ALBUM_MODIFIED_TIME				    TIMESTAMP(6)
+ ALBUM_LINK					    VARCHAR2(2000)
+ ALBUM_VISIBILITY				    VARCHAR2(100)
+ PHOTO_ID					    VARCHAR2(100)
+ PHOTO_CAPTION					    VARCHAR2(2000)
+ PHOTO_CREATED_TIME				    TIMESTAMP(6)
+ PHOTO_MODIFIED_TIME				    TIMESTAMP(6)
+ PHOTO_LINK					    VARCHAR2(2000)
+ */
+CREATE VIEW VIEW_PHOTO_INFORMATION (
+
+
+);
+
+/*
+ Name					   Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ PHOTO_ID					    VARCHAR2(100)
+ TAG_SUBJECT_ID 				    VARCHAR2(100)
+ TAG_CREATED_TIME				    TIMESTAMP(6)
+ TAG_X_COORDINATE				    NUMBER
+ TAG_Y_COORDINATE				    NUMBER
+ */
+CREATE VIEW VIEW_TAG_INFORMATION (PHOTO_ID
+
+
+);
+
+/*
+ Name					   Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ EVENT_ID					    VARCHAR2(100)
+ EVENT_CREATOR_ID				    VARCHAR2(100)
+ EVENT_NAME					    VARCHAR2(100)
+ EVENT_TAGLINE					    VARCHAR2(1000)
+ EVENT_DESCRIPTION				    VARCHAR2(4000)
+ EVENT_HOST					    VARCHAR2(100)
+ EVENT_TYPE					    VARCHAR2(100)
+ EVENT_SUBTYPE					    VARCHAR2(100)
+ EVENT_LOCATION 				    VARCHAR2(200)
+ EVENT_CITY					    VARCHAR2(100)
+ EVENT_STATE					    VARCHAR2(100)
+ EVENT_COUNTRY					    VARCHAR2(100)
+ EVENT_START_TIME				    TIMESTAMP(6)
+ EVENT_END_TIME 				    TIMESTAMP(6)
+ */
+CREATE VIEW VIEW_EVENT_INFORMATION (
+
+
+);
